4ebe4165299280480eaa83fdc30029eb5327240e
Slava Levkovich <slavlen1999@gmail.com>
2022-04-26 14:02:32 +0300

    chore(server): initial commit for heroku

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+    res.end('<h1>Home Page</h1>')
+})
+
+app.get('/about', (req, res) => {
+    res.end('<h1>About Page</h1>')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}`)
+})
